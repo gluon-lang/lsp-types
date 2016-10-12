9603be2c6ad6d1e290e986af2cd2ad02fea0aced
Markus Westerlind <marwes91@gmail.com>
2016-10-12 20:27:14 +0200

    fix: Make the serde generated code work with nightly/macros 1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
+#![cfg_attr(feature = "serde_derive", feature(proc_macro))]
+
+#[macro_use]
+extern crate enum_primitive;
 extern crate serde;
 extern crate serde_json;
 
-#[cfg(feature = "serde_macros")]
+#[cfg(feature = "serde_derive")]
+#[macro_use]
+extern crate serde_derive;
+
+#[cfg(feature = "serde_derive")]
 include!("lib.rs.in");
 
-#[cfg(not(feature = "serde_macros"))]
+#[cfg(feature = "serde_codegen")]
 include!(concat!(env!("OUT_DIR"), "/lib.rs"));
