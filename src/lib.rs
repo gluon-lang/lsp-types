95e0c2ce20b2e10b7279f476c84a2e3eef53fa8b
Markus Westerlind <marwes91@gmail.com>
2016-10-12 19:46:17 +0200

    refactor: Move the language-server types into a separate crate

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+extern crate serde;
+extern crate serde_json;
+
+#[cfg(feature = "serde_macros")]
+include!("lib.rs.in");
+
+#[cfg(not(feature = "serde_macros"))]
+include!(concat!(env!("OUT_DIR"), "/lib.rs"));
