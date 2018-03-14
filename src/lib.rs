f669ada0432768319ad4de2431c2faab34ec61b7
Junfeng Li <autozimu@gmail.com>
2018-03-13 21:41:21 -0700

    Add newer SymbolKind.
    Close #51.

--- src/lib.rs
+++ src/lib.rs
@@ -2033,6 +2033,14 @@ pub enum SymbolKind {
     Number = 16,
     Boolean = 17,
     Array = 18,
+    Object = 19,
+    Key = 20,
+    Null = 21,
+    EnumMember = 22,
+    Struct = 23,
+    Event = 24,
+    Operator = 25,
+    TypeParameter = 26,
 }
 }
 
