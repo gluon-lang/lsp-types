5deebe9106337db28471062aca53cc03f4f80bb0
Eyal Kalderon <ebkalderon@gmail.com>
2020-07-14 19:22:10 +0800

    Add HoverOptions, HoverRegistrationOptions, HoverProviderCapability

--- src/lib.rs
+++ src/lib.rs
@@ -1388,6 +1388,24 @@ pub struct CompletionOptions {
     pub work_done_progress_options: WorkDoneProgressOptions,
 }
 
+/// Hover options.
+#[derive(Debug, Eq, PartialEq, Clone, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct HoverOptions {
+    #[serde(flatten)]
+    pub work_done_progress_options: WorkDoneProgressOptions,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct HoverRegistrationOptions {
+    #[serde(flatten)]
+    pub text_document_registration_options: TextDocumentRegistrationOptions,
+
+    #[serde(flatten)]
+    pub hover_options: HoverOptions,
+}
+
 /// Signature help options.
 #[derive(Debug, Eq, PartialEq, Clone, Default, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -1569,6 +1587,13 @@ pub enum TypeDefinitionProviderCapability {
 
 #[derive(Debug, Eq, PartialEq, Clone, Deserialize, Serialize)]
 #[serde(untagged)]
+pub enum HoverProviderCapability {
+    Simple(bool),
+    Options(HoverOptions),
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, Deserialize, Serialize)]
+#[serde(untagged)]
 pub enum ColorProviderCapability {
     Simple(bool),
     ColorProvider(ColorProviderOptions),
@@ -1631,7 +1656,7 @@ pub struct ServerCapabilities {
 
     /// The server provides hover support.
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub hover_provider: Option<bool>,
+    pub hover_provider: Option<HoverProviderCapability>,
 
     /// The server provides completion support.
     #[serde(skip_serializing_if = "Option::is_none")]
