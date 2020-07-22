2e6eb8da251c9cf53a709dafce1b1c280d811336
Jeremy Kolb <kjeremy@gmail.com>
2020-07-22 08:56:21 -0400

    Add tag support

--- src/lib.rs
+++ src/lib.rs
@@ -947,6 +947,19 @@ pub struct WorkspaceSymbolClientCapabilities {
     /// Specific capabilities for the `SymbolKind` in the `workspace/symbol` request.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub symbol_kind: Option<SymbolKindCapability>,
+
+    /// The client supports tags on `SymbolInformation`.
+    /// Clients supporting tags have to handle unknown tags gracefully.
+    ///
+    /// @since 3.16.0
+    ///
+    #[serde(
+        default,
+        skip_serializing_if = "Option::is_none",
+        deserialize_with = "TagSupport::deserialize_compat"
+    )]
+    #[cfg(feature = "proposed")]
+    pub tag_support: Option<TagSupport<SymbolTag>>,
 }
 
 /// Workspace specific client capabilities.
@@ -2679,6 +2692,19 @@ pub struct DocumentSymbolClientCapabilities {
     /// The client support hierarchical document symbols.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub hierarchical_document_symbol_support: Option<bool>,
+
+    /// The client supports tags on `SymbolInformation`. Tags are supported on
+    /// `DocumentSymbol` if `hierarchicalDocumentSymbolSupport` is set to true.
+    /// Clients supporting tags have to handle unknown tags gracefully.
+    ///
+    /// @since 3.16.0
+    #[serde(
+        default,
+        skip_serializing_if = "Option::is_none",
+        deserialize_with = "TagSupport::deserialize_compat"
+    )]
+    #[cfg(feature = "proposed")]
+    pub tag_support: Option<TagSupport<SymbolTag>>,
 }
 
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
@@ -2728,8 +2754,14 @@ pub struct DocumentSymbol {
     pub detail: Option<String>,
     /// The kind of this symbol.
     pub kind: SymbolKind,
+    /// Tags for this completion item.
+    ///  since 3.16.0
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[cfg(feature = "proposed")]
+    pub tags: Option<Vec<SymbolTag>>,
     /// Indicates if this symbol is deprecated.
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[deprecated(note = "Use tags instead")]
     pub deprecated: Option<bool>,
     /// The range enclosing this symbol not including leading/trailing whitespace but everything else
     /// like comments. This information is typically used to determine if the the clients cursor is
@@ -2754,8 +2786,15 @@ pub struct SymbolInformation {
     /// The kind of this symbol.
     pub kind: SymbolKind,
 
+    /// Tags for this completion item.
+    ///  since 3.16.0
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[cfg(feature = "proposed")]
+    pub tags: Option<Vec<SymbolTag>>,
+
     /// Indicates if this symbol is deprecated.
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[deprecated(note = "Use tags instead")]
     pub deprecated: Option<bool>,
 
     /// The location of this symbol.
