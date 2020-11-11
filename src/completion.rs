411e3d806be7dce2ae2523391792a187983aabc6
Jeremy Kolb <kjeremy@gmail.com>
2020-11-11 10:00:52 -0500

    Add clarification from upstream

--- src/completion.rs
+++ src/completion.rs
@@ -149,11 +149,11 @@ pub enum InsertTextMode {
     AsIs = 1,
 
     /// The editor adjusts leading whitespace of new lines so that
-    /// they match the indentation of the line for which the item
-    /// is accepted.
+    /// they match the indentation up to the cursor of the line for
+    /// which the item is accepted.
     ///
-    /// For example if the line containing the cursor when a accepting
-    /// a multi line completion item is indented using 3 tabs all
+    /// Consider a line like this: <2tabs><cursor><3tabs>foo. Accepting a
+    /// multi line completion item is indented using 3 tabs all
     /// following lines inserted will be indented using 3 tabs as well.
     AdjustIndentation = 2,
 }
@@ -385,7 +385,7 @@ pub struct CompletionItem {
     pub insert_text_format: Option<InsertTextFormat>,
 
     /// How whitespace and indentation is handled during completion item insertion.
-    /// If ignored the clients default value depends on the `textDocument.completion.insertTextMode` client capability.
+    /// If not provided the clients default value depends on the `textDocument.completion.insertTextMode` client capability.
     #[cfg(feature = "proposed")]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub insert_text_mode: Option<InsertTextMode>,
