0d6acadefeef86648ac0e2b33453af79d195316e
Jeremy Kolb <kjeremy@gmail.com>
2020-11-12 10:00:11 -0500

    Adjust docs per review

--- src/completion.rs
+++ src/completion.rs
@@ -153,8 +153,8 @@ pub enum InsertTextMode {
     /// which the item is accepted.
     ///
     /// Consider a line like this: <2tabs><cursor><3tabs>foo. Accepting a
-    /// multi line completion item is indented using 3 tabs all
-    /// following lines inserted will be indented using 3 tabs as well.
+    /// multi line completion item is indented using 2 tabs all
+    /// following lines inserted will be indented using 2 tabs as well.
     AdjustIndentation = 2,
 }
 
