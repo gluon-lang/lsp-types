6e04b6b9c40b74f4dd9dcb724113f994d7df053c
Thomas Counsell <tamc@greenonblack.com>
2021-06-07 19:49:04 +0100

    Add constants for lsp specific error codes
    These are defined in https://microsoft.github.io/language-server-protocol/specifications//specification-3-17/#errorCodes

---
+++ src/error_codes.rs
@@ -0,0 +1,37 @@
+//! In this module we only define constants for lsp specific error codes.
+//! There are other error codes that are defined in the
+//! [JSON RPC specification](https://www.jsonrpc.org/specification#error_object).
+
+// This is the start range of LSP reserved error codes.
+// It doesn't denote a real error code.
+//
+// @since 3.16.0
+pub const lspReservedErrorRangeStart: i64 = -32899;
+
+// The server cancelled the request. This error code should
+// only be used for requests that explicitly support being
+// server cancellable.
+//
+// @since 3.17.0
+#[cfg(feature = "proposed")]
+pub const ServerCancelled: i64 = -32802;
+ 
+// The server detected that the content of a document got
+// modified outside normal conditions. A server should
+// NOT send this error code if it detects a content change
+// in it unprocessed messages. The result even computed
+// on an older state might still be useful for the client.
+//
+// If a client decides that a result is not of any use anymore
+// the client should cancel the request.
+pub const ContentModified: i64 = -32801;
+
+// The client has canceled a request and a server as detected
+// the cancel.
+pub const RequestCancelled: i64 = -32800;
+
+// This is the end range of LSP reserved error codes.
+// It doesn't denote a real error code.
+//
+// @since 3.16.0
+pub const lspReservedErrorRangeEnd: i64 = -32800;
