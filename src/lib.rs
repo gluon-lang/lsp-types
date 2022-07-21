fef1f064987b29ebf465bb35f69938900103f230
MinusGix <MinusGix@gmail.com>
2022-07-21 09:52:05 -0500

    Skip serializing link support if it is None
    Currently if this isn't specified (such as being left to `GotoCapability::default()`) then it will be serialized as `Null`. The Julia LSP will panic if it sees that field without a boolean value.  
    This PR updates the field to be skipped, matching the specification.

--- src/lib.rs
+++ src/lib.rs
@@ -1099,6 +1099,7 @@ pub struct GotoCapability {
     pub dynamic_registration: Option<bool>,
 
     /// The client supports additional metadata in the form of definition links.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub link_support: Option<bool>,
 }
 
