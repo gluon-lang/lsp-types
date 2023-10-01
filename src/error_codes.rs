4645559f78e5430d2bc1d1cb95518aae3889d536
gibbz00 <gabrielhansson00@gmail.com>
2023-10-01 18:11:58 +0200

    Add missing error codes.

--- src/error_codes.rs
+++ src/error_codes.rs
@@ -2,11 +2,29 @@
 //! There are other error codes that are defined in the
 //! [JSON RPC specification](https://www.jsonrpc.org/specification#error_object).
 
+// Defined in the LSP specification but in the range reserved for JSON-RPC error codes,
+// namely the -32099 to -32000 "Reserved for implementation-defined server-errors." range.
+// The code has, nonetheless, been left in this range for backwards compatibility reasons.
+pub const SERVER_NOT_INITIALIZED: i64 = -32002;
+
+// Defined in the LSP specification but in the range reserved for JSON-RPC error codes,
+// namely the -32099 to -32000 "Reserved for implementation-defined server-errors." range.
+// The code has, nonetheless, left in this range for backwards compatibility reasons.
+pub const UNKNOWN_ERROR_CODE: i64 = -32001;
+
 // This is the start range of LSP reserved error codes.
 // It doesn't denote a real error code.
 //
 // @since 3.16.0
 pub const LSP_RESERVED_ERROR_RANGE_START: i64 = -32899;
+
+// A request failed but it was syntactically correct, e.g the
+// method name was known and the parameters were valid. The error
+// message should contain human readable information about why
+// the request failed.
+//
+//@since 3.17.0
+pub const REQUEST_FAILED: i64 = -32803;
 
 // The server cancelled the request. This error code should
 // only be used for requests that explicitly support being
